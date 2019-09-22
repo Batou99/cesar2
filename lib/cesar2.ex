e01e5611d625d0c9ab0b58d9a69ce4bb5e634aae
Lorenzo Lopez Sancho <lorenzo.lopez@intec.es>
2019-09-22 11:20:04 +0200

    Finish encrypt/decrypt

--- lib/cesar2.ex
+++ lib/cesar2.ex
@@ -2,13 +2,18 @@ defmodule Cesar2 do
   @moduledoc """
   Documentation for Cesar2.
   """
+
+  @low ?!
+  @high ?~
+  @printable_range @low..@high
+
   @spec encrypt(binary() | [char()], integer()) :: binary() | [char()]
   def encrypt(input, key)
 
   def encrypt([], key), do: []
 
   def encrypt([head | tail], key) do
-    []
+    [shift(head, key) | encrypt(tail, key)]
   end
 
   def encrypt(string, key) do
@@ -21,5 +26,40 @@ defmodule Cesar2 do
   @spec decrypt(binary(), integer()) :: binary()
   def decrypt(string, key) do
     encrypt(string, -key)
+  end
+  
+  @spec normalize(char()) :: char()
+  def normalize(char) do
+    char - @low
+  end
+
+  @spec denormalize(char()) :: char()
+  def denormalize(char) do
+    char + @low
+  end
+
+  @spec shift(char(), integer()) :: char()
+  def shift(char, amount)
+
+  def shift(char, amount) when char in @printable_range do
+    (char + amount)
+    |> normalize
+    |> modulo(Enum.count(@printable_range))
+    |> denormalize
+  end
+
+  def shift(char, _amount) do
+    char
+  end
+
+  def low, do: @low
+  def high, do: @high
+
+  defp modulo(n, q) when is_integer(n) and n == abs(n) do
+    rem(n, q)
+  end
+
+  defp modulo(n, q) when is_integer(n) do
+    rem(n, q) + q
   end
 end
--- test/cesar2_test.exs
+++ test/cesar2_test.exs
@@ -3,11 +3,50 @@ defmodule Cesar2Test do
   use PropCheck
   doctest Cesar2
 
-  property "inverse" do
-    forall {text, key} <- {binary(), integer()} do
-      text
-      |> Cesar2.encrypt(key)
-      |> Cesar2.decrypt(key) == text
+  def printable_string do
+    printable_charlist_gen = such_that s <- char_list(), when: :io_lib.printable_latin1_list(s)
+
+    let s <- printable_charlist_gen, do: to_string(s)
+  end
+
+  def shiftable_char do
+    integer(Cesar2.low, Cesar2.high)
+  end
+
+  property "encrypt-decrypt: inverse", [{:constraint_tries, 500}, {:numtests, 1000}] do
+    forall {text, key} <- {printable_string(), integer()} do
+      (text |> Cesar2.encrypt(key) |> Cesar2.decrypt(key)) == text
+    end
+  end
+
+  describe "Normalize/Denormalize" do
+    property "value" do
+      forall {letter} <- {shiftable_char()} do
+        Cesar2.normalize(letter) <= Cesar2.high-Cesar2.low
+      end
+    end
+
+    test "first to zero" do
+      assert Cesar2.normalize(Cesar2.low) == 0
+    end
+
+    test "zero to first" do
+      assert Cesar2.denormalize(0) == Cesar2.low
+    end
+
+    property "inverse" do
+      forall {letter} <- {integer()} do
+        (Cesar2.normalize(letter) |> Cesar2.denormalize) == letter &&
+          (Cesar2.denormalize(letter) |> Cesar2.normalize) == letter
+      end
+    end
+  end
+
+  describe "shift" do
+    property "inverse" do
+      forall {letter, key} <- {integer(), integer()} do
+        Cesar2.shift(letter, key) |> Cesar2.shift(-key) == letter
+      end
     end
   end
 end
